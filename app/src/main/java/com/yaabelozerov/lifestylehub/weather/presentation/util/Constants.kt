2fb2da258cabec01a1ba94b0f07c28a055435e35
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-03-20 18:53:41 +0000

    Restructured weather widget ui 2

--- app/src/main/java/com/yaabelozerov/lifestylehub/weather/presentation/util/Constants.kt
+++ app/src/main/java/com/yaabelozerov/lifestylehub/weather/presentation/util/Constants.kt
@@ -3,6 +3,7 @@ package com.yaabelozerov.lifestylehub.weather.presentation.util
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 sealed class Constants {
     sealed class Shimmer {
@@ -15,6 +16,13 @@ sealed class Constants {
                 Color.LightGray.copy(alpha = 0.9f)
             )
             val shape = RoundedCornerShape(8.dp)
+        }
+    }
+
+    sealed class Fonts {
+        companion object {
+            val small = 12.sp
+            val medium = 20.sp
         }
     }
 }
