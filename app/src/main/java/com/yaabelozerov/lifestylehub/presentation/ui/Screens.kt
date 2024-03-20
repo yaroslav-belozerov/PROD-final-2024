2f306f3c9ac8dd31857ae6b4a4ddd50262b13065
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-03-20 19:09:49 +0000

    Fullfill previous commit

---
+++ app/src/main/java/com/yaabelozerov/lifestylehub/presentation/ui/Screens.kt
@@ -0,0 +1,8 @@
+package com.yaabelozerov.lifestylehub.presentation.ui
+
+sealed class Screens(val route: String) {
+    object MainScreen: Screens("MainScreen")
+    object NotesScreen: Screens("NotesScreen")
+    object ProfileScree: Screens("ProfileScreen")
+    object DetailsScreen: Screens("DetailsScreen")
+}
---
+++ app/src/main/java/com/yaabelozerov/lifestylehub/weather/data/remote/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.yaabelozerov.lifestylehub.weather.data.remote.util
+
+import kotlin.math.sign
+
+sealed class Constants {
+    companion object {
+        const val OWM_ICON_URL = "https://openweathermap.org/img/wn/"
+        const val OWM_ICON_PARAMETERS = "@4x.png"
+        const val OWM_ICON_PLACEHOLDER = "10d"
+    }
+}
