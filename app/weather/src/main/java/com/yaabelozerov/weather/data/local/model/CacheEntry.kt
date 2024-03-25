25f6aa6c4a0d1000d7b2eefd8bc0bf7ef8dc53c0
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-03-25 17:11:40 +0000

    Removed weather caching

--- app/weather/src/main/java/com/yaabelozerov/weather/data/local/model/CacheEntry.kt
+++
@@ -1,9 +0,0 @@
-package com.yaabelozerov.weather.data.local.model
-
-import java.time.LocalDateTime
-
-interface CacheEntry<T> {
-    val id: String
-    val expiresAt: LocalDateTime
-    val data: T
-}
--- app/weather/src/main/java/com/yaabelozerov/weather/data/repository/WeatherRepositoryImpl.kt
+++ app/weather/src/main/java/com/yaabelozerov/weather/data/repository/WeatherRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package com.yaabelozerov.weather.data.repository
 
 import com.yaabelozerov.common.domain.Resource
-import com.yaabelozerov.weather.data.local.model.CacheRegistry
-import com.yaabelozerov.weather.data.local.model.WeatherDataCacheEntry
 import com.yaabelozerov.weather.data.remote.mapper.OwmWeatherToDomainMapper
 import com.yaabelozerov.weather.data.remote.source.OwmWeatherApi
 import com.yaabelozerov.weather.domain.model.WeatherData
@@ -13,8 +11,7 @@ import javax.inject.Inject
 class WeatherRepositoryImpl
     @Inject
     constructor(
-        private val owmApi: OwmWeatherApi,
-        private val cacheRegistry: CacheRegistry<WeatherDataCacheEntry>,
+        private val owmApi: OwmWeatherApi
     ) : WeatherRepository {
         override suspend fun getWeatherData(
             lat: Double,
@@ -26,27 +23,6 @@ class WeatherRepositoryImpl
                 val dto = owmApi.weatherByCoordinates(lat, lon, lang).await()
                 val data = mapper.mapToDomainModel(dto)
                 Resource.Success(data = data)
-
-//            val cached = cacheRegistry.getEntry("${lat.toInt()}_${lon.toInt()}$lang")
-//            val cacheEntryMapper =
-//                com.yaabelozerov.weather.data.local.mapper.WeatherDataCacheEntryMapper()
-//            if (cached == null) {
-//                val mapper = OwmWeatherToDomainMapper()
-//                val dto = owmApi.weatherByCoordinates(lat, lon, lang).await()
-//                cacheRegistry.putEntry(
-//                    "${lat.toInt()}_${lon.toInt()}$lang", cacheEntryMapper.mapToCacheEntry(
-//                        mapper.mapToDomainModel(
-//                            dto
-//                        )
-//                    )
-//                )
-//                val data = mapper.mapToDomainModel(dto)
-//                Log.d("WeatherRepositoryImpl", "getWeatherData: $data")
-//                Resource.Success(data = data)
-//
-//            } else {
-//                Resource.Success(cacheEntryMapper.mapToDomainModel(cached))
-//            }
             } catch (e: Exception) {
                 e.printStackTrace()
                 Resource.Error(e.message ?: "Unexpected error")
